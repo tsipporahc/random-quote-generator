f47d61b0509f27c04866de9ba93911afd4b067b4
tsipporahc <tsipporahc@gmail.com>
2022-08-27 21:52:08 -0700

    able to pass printFunction
    complete the second function, and fix html tags

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Random Quote Generator
+
+Breaking it down
+
+1. How do I generate a random number?
+
+2. How do I use that random number to get a quote object from the quotes array?
+
+ideas-
+randomly select a quote from the array and then return it
+    function getRandomQuote() {
+
+        // Code to get quote from array
+    }
+
+
+
+call getRandomQuote and then print it    
+function printQuote(){
+
+        // Code to print the quote using fron project instructions
+    }
+
+Comments in code about what it is doing and how it works.
--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+    {quote:'Success is liking yourself, liking what you do, and liking how you do it.', source:'Maya Angelou'},
+    {quote:'I was built this way for a reason, so I will use it.', source:'Simone Biles', year:'2020'},
+    {quote:'Your silence will not protect you.', source:'Audre Lorde', citation: 'The Transformation of Silence into Language and Action'},
+    {quote:'If we give our children sound self-love, they will be able to deal with whatever life puts before them.', source:'Bell Hooks'},
+    {quote:'It is not the load that breaks you down; it is the way you carry it.', source:'Lena Horne'},
+    {quote:'No matter what accomplishments you make, somebody helped you.', source:'Althena Gibson'}
+  ];
+
+
 
 
 /***
@@ -18,11 +28,54 @@ project 1 - A Random Quote Generator
 ***/
 
 
+let getRandomQuote = (arr) => {
+  let random = Math.floor(Math.random() * (quotes.length));
+  random = quotes[random]; // i want to grab the values only ?
+
+  return random;
+};
+
+
+getRandomQuote(quotes);
+
+
 
 /***
  * `printQuote` function
 ***/
 
+
+let printQuote = () => {
+  let randomQuote = getRandomQuote(); // do i need parameter here?
+  let html = ''; 
+  html +=
+  `
+  <p class="quote">${randomQuote.quote}</p>
+  <p class="source"> ${randomQuote.source}
+  `;
+  if (randomQuote.citation) { // If the random quote object has a citation property, concatenate a <span> element with the class "citation" to the HTML string.
+   html += `
+    <span class="citation">${randomQuote.citation}</span>
+    `;
+  } 
+
+  if (randomQuote.year) { // If the random quote object has a year property, concatenate a <span> element with the class "year" to the HTML string.
+  html += `
+    <span class="year">${randomQuote.year}</span>
+    `;
+  
+  }
+ 
+  html += `
+ </p>
+ `; 
+    
+  document.getElementById('quote-box').innerHTML = html; 
+
+
+};
+
+printQuote();
 
 
 /***
